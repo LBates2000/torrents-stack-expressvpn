49c617f94ec71d7ef0026960d7618f4e353d1c63
Lawrence Bates <Lawrence.Bates@gmail.com>
2026-04-02 18:57:29 -0700

    fix: simplify shared function test assertion

--- tests/shared-functions.Tests.ps1
+++ tests/shared-functions.Tests.ps1
@@ -2,16 +2,8 @@
 
 Import-Module "$PSScriptRoot/../scripts/shared-functions.ps1"
 
-$pesterModule = Get-Module -Name Pester
-$useModernShouldSyntax = $pesterModule -and $pesterModule.Version.Major -ge 5
-
 Describe "Write-ProgressLine" {
     It "Should write output without error in non-interactive session" {
-        $action = { Write-ProgressLine "Test message" -Color Yellow }
-        if ($useModernShouldSyntax) {
-            $action | Should -Not -Throw
-        } else {
-            $action | Should Not Throw
-        }
+        Write-ProgressLine "Test message" -Color Yellow
     }
 }
