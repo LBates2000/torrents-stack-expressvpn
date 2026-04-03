5b8dcfe80f1f6bb430b15264770be4f1fb52a671
Lawrence Bates <Lawrence.Bates@gmail.com>
2026-04-02 18:56:12 -0700

    fix: support Pester 3 and 5 test syntax

--- tests/shared-functions.Tests.ps1
+++ tests/shared-functions.Tests.ps1
@@ -2,8 +2,16 @@
 
 Import-Module "$PSScriptRoot/../scripts/shared-functions.ps1"
 
+$pesterModule = Get-Module -Name Pester
+$useModernShouldSyntax = $pesterModule -and $pesterModule.Version.Major -ge 5
+
 Describe "Write-ProgressLine" {
     It "Should write output without error in non-interactive session" {
-        { Write-ProgressLine "Test message" -Color Yellow } | Should Not Throw
+        $action = { Write-ProgressLine "Test message" -Color Yellow }
+        if ($useModernShouldSyntax) {
+            $action | Should -Not -Throw
+        } else {
+            $action | Should Not Throw
+        }
     }
 }
